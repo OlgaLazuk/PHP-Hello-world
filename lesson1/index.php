c0a552059313df98bfdcbc193b94c13bcfcde8c9
olgalazuk <83114051+olgalazuk@users.noreply.github.com>
2021-07-08 10:17:02 +0300

    Add files via upload

---
+++ lesson1/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 5;
+$bool = true;
+$b = 2.34;
+$name = "Olga";
+$arr = ["student1", "student2",];
+echo $a, $bool, $b, $name, $arr;
+
+$c = $a + $b;
+$d = $a - $b;
+$x = $a * $b;
+$y = $a % $b;
+$z = $a / $b;
+var_dump($c, $d, $x, $y, $z);
+
+var_dump(5 > 4);
+var_dump(2 == 2);
+var_dump(10 < 7);
+var_dump(3 >= 2);
+var_dump(122 != "text");
+
